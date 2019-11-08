18532ad865f16759e01e332f7afba037ba49b638
Mashayikh <shakmash@gmail.com>
2019-11-08 15:54:35 +0530

    Added function to write to the textbox

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-// Code goes here
 var calculationVars={
   addNum : function(a,b)
   {
     return(a+b);
   }
+}
+function num1()
+{  var inputNumber = document.getElementById("firstName");
+inputNumber.value='1';
 }
